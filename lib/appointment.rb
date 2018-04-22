c9a7779a9035a3289505f4bd55c9732ff661712e
Jonathan Williams <jtw365@live.missouristate.edu>
2018-04-22 22:37:46 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,12 +1,14 @@
 require 'pry'
 class Appointment
 
-  attr_accessor :name, :doctor, :patient
+  attr_accessor :name, :patient
 
   def initialize(name, patient)
     @name = name
     @patient = patient
     patient.add_appointment(self)
   end
+
+  def
 
 end
